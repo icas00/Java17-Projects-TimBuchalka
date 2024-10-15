81ba35149225f0a661ab83c94aa77d9be1a6624c
icas00 <121344315+icas00@users.noreply.github.com>
2024-10-15 23:03:08 +0530

    Update DecimalComparator.java

--- DecimalComparator.java
+++ DecimalComparator.java
@@ -1,14 +1,14 @@
 public class DecimalComparator {
     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2){
-        long Round1 = (long) (num1 * 1000);
+        long Round1 = (long) (num1 * 1000);//Create a new long Round1 and convert num1 to long after multiplying by 1000
         long Round2 = (long) (num2 * 1000);
         if (Round1 == Round2){
             System.out.println("Equal");
             return true;
         }else{
             System.out.println("Not Equal");
-            return false;}
-
+            return false;
+        }
     }
 
     public static void main(String[] args){
